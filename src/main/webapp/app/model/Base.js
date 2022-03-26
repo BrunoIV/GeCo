88d247f8af39ffc31d558cd883663ea42f13665b
Bruno Expósito <bruno.exposito@gmail.com>
2022-03-26 13:52:03 +0000

    Agrego el frontend en sencha7

---
+++ src/main/webapp/app/model/Base.js
@@ -0,0 +1,7 @@
+Ext.define('GeCo.model.Base', {
+    extend: 'Ext.data.Model',
+
+    schema: {
+        namespace: 'GeCo.model'
+    }
+});
---
+++ src/main/webapp/app/model/Personnel.js
@@ -0,0 +1,7 @@
+Ext.define('GeCo.model.Personnel', {
+    extend: 'GeCo.model.Base',
+
+    fields: [
+        'name', 'email', 'phone'
+    ]
+});
