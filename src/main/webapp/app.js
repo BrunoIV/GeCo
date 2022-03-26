debf96d9d698ef6a925c8a9a7ca3c165bbaf7695
Bruno Expósito <bruno.exposito@gmail.com>
2022-03-26 16:24:10 +0000

    Agrego a app.js dependencias para los formularios

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -10,6 +10,20 @@ Ext.application({
     requires: [
         // This will automatically load all classes in the GeCo namespace
         // so that application classes do not need to require each other.
+        'Ext.data.BufferedStore',
+        'Ext.container.Viewport',
+        'Ext.Img',
+        'Ext.form.*',
+        'Ext.form.field.*',
+        'Ext.layout.container.*',
+        'Ext.panel.*',
+        'Ext.picker.*',
+        'Ext.slider.*',
+        'Ext.toolbar.*',
+        'Ext.util.Cookies',
+        'Ext.util.History',
+        'Ext.ux.statusbar.StatusBar',
+        'Ext.ux.TabReorderer',
         'GeCo.*'
     ],
 
