788eeeb43cd28cb2d5e3acdbb2c17312c397c4f3
Bruno Expósito <bruno.exposito@gmail.com>
2022-03-27 17:16:48 +0000

    Agrego función goDialog a Util.js

--- src/main/webapp/app/Util.js
+++ src/main/webapp/app/Util.js
@@ -6,5 +6,29 @@ Ext.define('GeCo.Util', {
 		
 		let tab = Ext.widget(xtype);
 		tabContainer.add(tab);
-	}
+	},
+	
+	goDialog: function(xtype, extraParams) {
+		var panel = Ext.widget(xtype, {
+			preventHeader: true //evita doble título
+		});
+
+		// Crea dialogo con panel dentro, heredando el título e icono del panel
+		var window = Ext.create('Ext.window.Window', {
+			title: panel.title,
+			iconCls: panel.iconCls,
+			items: panel,
+			modal: true,
+			resizable: false,
+			layout: 'fit',
+			height: (typeof panel.height === 'undefined' ? '70%' : panel.height),
+			width: (typeof panel.width === 'undefined' ? '70%' : panel.width)
+		});
+	
+			
+		// Aplica los parámetros extra
+		if (extraParams !== null) Ext.apply(panel, extraParams);
+    	
+    	window.show();
+    },
 });
