6789fd60e40464f347360c10daaeeedc3ffcbad0
Bruno Expósito <bruno.exposito@gmail.com>
2022-03-27 17:18:59 +0000

    Agrego al viewmodel las columnas por defecto de NewListCenter

--- src/main/webapp/app/view/objects/list/NewListTabModel.js
+++ src/main/webapp/app/view/objects/list/NewListTabModel.js
@@ -1,4 +1,13 @@
 Ext.define('GeCo.view.objects.list.NewListTabModel', {
     extend: 'Ext.app.ViewModel',
-    alias: 'viewmodel.NewListTab'
+    alias: 'viewmodel.NewListTab',
+    
+	data: {
+		default_list_center_columns: [{
+			xtype: 'gridcolumn',
+			dataIndex: 'id',
+			text: 'Id'
+		}]
+	}
+
 });
